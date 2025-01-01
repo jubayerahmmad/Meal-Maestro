ba8618524d17444c57ec44c5a0e06b7788d0c7ed
jubayerahmmad <zubayersami510@gmail.com>
2025-01-01 19:20:21 +0600

    add to cart

--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -32,6 +32,7 @@ async function run() {
     const db = client.db("maestroDB");
     const menuCollection = db.collection("menus");
     const reviewsCollection = db.collection("reviews");
+    const cartsCollection = db.collection("carts");
 
     // get menu data
     app.get("/menus", async (req, res) => {
@@ -41,6 +42,14 @@ async function run() {
     // get reviews data
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // CARTS
+    // save carts
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
       res.send(result);
     });
   } finally {
