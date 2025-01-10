d21022d46a6214fa1bae12fc1ec6b8cf44666efe
jubayerahmmad <zubayersami510@gmail.com>
2025-01-10 08:02:35 +0600

    admin home statistics

--- bistro-boss-client/src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ bistro-boss-client/src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,10 +1,188 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaDollarSign, FaList, FaUsers } from "react-icons/fa6";
+import { FaListAlt } from "react-icons/fa";
+import { BsMenuAppFill } from "react-icons/bs";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  PieChart,
+  Pie,
+  Legend,
+} from "recharts";
+
+// barchart color
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+// piechart color
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+// custom chart
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${
+    y + height / 3
+  }
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+    x + width
+  }, ${y + height}
+  Z`;
+};
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+// piechart custom
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 
 const AdminHome = () => {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: stats = {} } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const { data } = await axiosSecure("/admin-stats");
+      return data;
+    },
+  });
+
+  const { data: chartData = [] } = useQuery({
+    queryKey: ["order-stats"],
+    queryFn: async () => {
+      const { data } = await axiosSecure("/order-stats");
+      return data;
+    },
+  });
+
+  const pieChartData = chartData.map((data) => {
+    return { name: data?.category, value: data?.revenue };
+  });
+
+  const { users, orders, menuItems, revenue } = stats;
   return (
     <div>
-      <h2>Hi, Welcome Back {user?.displayName && user?.displayName}!</h2>
+      <h2 className="text-4xl font-semibold">
+        Hi, Welcome Back {user?.displayName && user?.displayName}!
+      </h2>
+
+      <div className="stats stats-vertical lg:stats-horizontal shadow w-full my-6">
+        <div className="stat space-y-4">
+          <div className="stat-title text-xl">Users</div>
+          <div className="stat-value flex items-center gap-3">
+            <FaUsers /> {users}
+          </div>
+        </div>
+
+        <div className="stat space-y-4">
+          <div className="stat-title text-xl">Orders</div>
+          <div className="stat-value flex items-center gap-3">
+            <FaListAlt /> {orders}
+          </div>
+        </div>
+
+        <div className="stat space-y-4">
+          <div className="stat-title text-xl">Menu Items</div>
+          <div className="stat-value flex items-center gap-3">
+            {" "}
+            <BsMenuAppFill /> {menuItems}
+          </div>
+        </div>
+
+        <div className="stat space-y-4">
+          <div className="stat-title text-xl">Revenue</div>
+          <div className="stat-value flex items-center gap-3">
+            {" "}
+            <FaDollarSign /> {revenue}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-4">
+        {/* barchart */}
+        <div>
+          <BarChart
+            width={500}
+            height={300}
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Bar
+              dataKey="quantity"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </div>
+        {/* piechart */}
+        <div>
+          <PieChart width={400} height={400}>
+            <Pie
+              data={pieChartData}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {pieChartData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Legend></Legend>
+          </PieChart>
+        </div>
+      </div>
     </div>
   );
 };
